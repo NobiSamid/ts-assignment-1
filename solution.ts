86cda88c939f3ad29816fbe5cabd91d143a4f666
NobiSamid <nobisamid@gmail.com>
2025-11-28 17:10:17 +0600

    problem 7 done

--- solution.ts
+++ solution.ts
@@ -55,13 +55,13 @@ class Person{
 // const person2 = new Person('Alice', 25);
 // console.log(person2.getDetails());
 
-interface Books {
+interface Products {
   title: string;
   rating: number;
 }
 
-const filterByRating = (books: Books[]) : Books[] =>{
-  return books.filter(book => book.rating >= 4.0).map(book => ({...book, rating: Number(book.rating.toFixed(1))}))
+const filterByRating = (products: Products[]) : Products[] =>{
+  return books.filter(product => product.rating >= 4.0).map(product => ({...product, rating: Number(product.rating.toFixed(1))}))
 }
 
 
@@ -71,5 +71,79 @@ const books = [
   { title: 'Book C', rating: 5.0 },
 ];
 
-console.log(filterByRating(books));
+// console.log(filterByRating(books));
+
+
+interface User{
+  id: number;
+  name: string;
+  email: string;
+  isActive: boolean;
+}
+
+const filterActiveUsers = (users: User[]) : User[] =>{
+  return users.filter( user => user.isActive === true)
+}
+
+const users = [
+  { id: 1, name: 'Rakib', email: 'rakib@example.com', isActive: true },
+  { id: 2, name: 'Asha', email: 'asha@example.com', isActive: false },
+  { id: 3, name: 'Rumi', email: 'rumi@example.com', isActive: true },
+];
+
+// console.log(filterActiveUsers(users));
+
+
+interface Book{
+  title: string
+  author: string;
+  publishedYear: number;
+  isAvailable: boolean;
+}
+
+const printBookDetails = (book: Book) =>{
+  let availability = 'No'
+  if (book.isAvailable == true){
+    availability = 'Yes'
+  }
+  console.log(`Title: ${book.title}, Author: ${book.author}, Published: ${book.publishedYear}, Available: ${availability}`)
+}
+
+const myBook: Book = {
+  title: 'The Great Gatsby',
+  author: 'F. Scott Fitzgerald',
+  publishedYear: 1925,
+  isAvailable: true,
+};
+
+// printBookDetails(myBook);
+
+
+const getUniqueValues = (arr1: any[], arr2: any[] ) =>{
+  const result: any[] = [...arr1, ...arr2]
+  const uniqueArr: any[] = []
+
+  for (let i = 0; i < result.length; i++){
+    let isDuplicate = false;
+
+    for (let j=0; j < uniqueArr.length; j++){
+      if (result[i] === uniqueArr[j]){
+        isDuplicate = true;
+        break
+      }
+    }
+
+    if(!isDuplicate){
+      uniqueArr.push(result[i])
+    }
+  }
+  return uniqueArr;
+}
+
+// const array1 = [1, 2, 3, 4, 5, true, 'cat', false, 'cat'];
+// const array2 = [3, 4, 5, 6, 7, false];
+const array1 = [1, 2, 3, 4, 5];
+const array2 = [3, 4, 5, 6, 7];
+console.log(getUniqueValues(array1, array2));
+
 
